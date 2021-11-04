838c25df5c352efe09b6c286c50c2d9caab047b3
Nicolas <nberr@eonerc.rwth-aachen.de>
2021-11-04 15:20:53 +0100

    added ci code

--- test.sh
+++ test.sh
@@ -1,3 +1,7 @@
 echo "dies ist ein lokaler test, der einen Trigger ausloesen soll (tach, test)"
 echo "Verzeichnis: "`pwd`
 echo "Zeitstempel: "`date`
+
+java -Djava.ext.dirs=library mdw2021/CompetitionMainClass --t1
+java -Djava.ext.dirs=library mdw2021/CompetitionMainClass --t2 "Test Text zum Argument testen"
+java -Djava.ext.dirs=library mdw2021/CompetitionMainClass --t3 
