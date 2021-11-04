de3a0ae389496db089ff49f2270f17aa0b2512cf
Nicolas <nberr@eonerc.rwth-aachen.de>
2021-11-04 15:41:17 +0100

    next try form compile

--- test.sh
+++ test.sh
@@ -1,7 +1,8 @@
+LIB=library/commons-cli-1.5.0.jar
 echo "dies ist ein lokaler test, der einen Trigger ausloesen soll (tach, test)"
 echo "Verzeichnis: "`pwd`
 echo "Zeitstempel: "`date`
 
-java -Djava.ext.dirs=library mdw2021/CompetitionMainClass --t1
-java -Djava.ext.dirs=library mdw2021/CompetitionMainClass --t2 "Test Text zum Argument testen"
-java -Djava.ext.dirs=library mdw2021/CompetitionMainClass --t3 
+java -cp $LIB:. mdw2021/CompetitionMainClass --t1 
+java -cp $LIB:. mdw2021/CompetitionMainClass --t2 "Test Text zum Argument testen"
+java -cp $LIB:. mdw2021/CompetitionMainClass --t3 
