b2a261f8750f61c7521ee68a418659157e879eb4
Nicolas <nberr@eonerc.rwth-aachen.de>
2021-11-09 10:29:57 +0100

    changed to jar file compilation

--- compile.sh
+++ compile.sh
@@ -1,1 +1,2 @@
-javac -classpath library/commons-cli-1.5.0.jar mdw2021/CompetitionMainClass.java
+javac -cp library/commons-cli-1.5.0.jar mdw2021/CompetitionMainClass.java
+jar cfme md2021.jar manifest.txt mdw2021.CompetitionMainClass mdw2021/*.class
--- test.sh
+++ test.sh
@@ -1,8 +1,7 @@
-LIB=library/commons-cli-1.5.0.jar
 echo "dies ist ein lokaler test, der einen Trigger ausloesen soll (tach, test)"
 echo "Verzeichnis: "`pwd`
 echo "Zeitstempel: "`date`
 
-java -cp $LIB:. mdw2021/CompetitionMainClass --t1 
-java -cp $LIB:. mdw2021/CompetitionMainClass --t2 "Test Text zum Argument testen"
-java -cp $LIB:. mdw2021/CompetitionMainClass --t3 
+java -jar md2021.jar --t1
+java -jar md2021.jar --t2 "Test Text zum Argument testen"
+java -jar md2021.jar --t3
