837a691ad21ca9942e2bd6113a82bb417addfc3a
Georg Adalbert Heinrich Schramm <schramm@itc.rwth-aachen.de>
2021-11-11 22:17:37 +0100

    Neue Datei hochladen

---
+++ mdw2021/InputFileSyntaxChecker.java
@@ -0,0 +1,7 @@
+package mdw2021;
+
+public class InputFileSyntaxChecker {
+	static void CheckSyntax(String inputFile) throws Exception {
+		throw new Exception("Input file checker not implemented!");
+	}
+}
