33e5871720c7cbb4e404f7f094c1aa1c1d871937
Georg Adalbert Heinrich Schramm <schramm@itc.rwth-aachen.de>
2021-11-11 22:18:00 +0100

    Neue Datei hochladen

---
+++ mdw2021/ResultFileSyntaxChecker.java
@@ -0,0 +1,7 @@
+package mdw2021;
+
+public class ResultFileSyntaxChecker {
+	static void CheckSyntax(String resultFile) throws Exception {
+		throw new Exception("Result file checker not implemented!");
+	}
+}
