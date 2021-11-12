06388887d635721d8c02f03ac2b89be4bd138635
Georg Adalbert Heinrich Schramm <schramm@itc.rwth-aachen.de>
2021-11-12 22:42:25 +0100

    Update SemanticsChecker.java

--- mdw2021/SemanticsChecker.java
+++ mdw2021/SemanticsChecker.java
@@ -2,6 +2,13 @@ package mdw2021;
 
 public class SemanticsChecker {
 	static boolean checkInputOutpuConsistency(String input, String output, boolean verboseOutput) {
+		//check if the dimensions for input and output are equal
+		//check number of cubes
+
+		//check if all cubes have an equivalent
+		//build a representation for a cube
+		//implement method to rotate a cube through all possible positions  
+		
 		return false;
 	}
 }
