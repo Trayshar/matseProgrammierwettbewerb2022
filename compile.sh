b87d3d7f31f46c82ffb985a32186578838a3a288
Georg Adalbert Heinrich Schramm <schramm@itc.rwth-aachen.de>
2021-11-14 14:28:02 +0100

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,2 +1,2 @@
-javac -cp library/commons-cli-1.5.0.jar mdw2021/CompetitionMainClass.java mdw2021/InputFileSyntaxChecker.java mdw2021/ResultFileSyntaxChecker.java mdw2021/Cube.java mdw2021/SemanticsChecker.java
+javac -cp library/commons-cli-1.5.0.jar mdw2021/CompetitionMainClass.java 
 jar cfme md2021.jar manifest.txt mdw2021.CompetitionMainClass mdw2021/*.class
