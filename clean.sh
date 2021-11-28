3e0d01f309c1aab6fe0a65901bd8b74035c990de
Nicolas <nberr@eonerc.rwth-aachen.de>
2021-11-28 14:50:46 +0100

    compile and clean now more independent from directory structure

--- clean.sh
+++ clean.sh
@@ -1,3 +1,13 @@
 rm -f md2021.jar
-rm -f src/mdw2021/*.class
-rm -f src/implementation/*.class
+
+## unfortunately find is not supported by ci image
+# CLASS=`find . -name "*.class"`
+## so we have to missuse jar as a workaround
+jar cf temp.jar .
+CLASS=`jar tf temp.jar | grep ".class"`
+rm temp.jar
+## end workaround
+
+if test -n "$CLASS"; then
+	rm $CLASS
+fi
--- compile.sh
+++ compile.sh
@@ -1,4 +1,23 @@
 cd src
-javac -cp ../library/commons-cli-1.5.0.jar mdw2021/*.java implementation/*.java
-jar cfme ../md2021.jar ../manifest.txt mdw2021.CompetitionMainClass mdw2021/*.class implementation/*.class
+
+## unfortunately find is not supported by ci image
+# SRC=`find . -name "*.java"`
+## so we have to missuse jar as a workaround
+jar cf temp.jar .
+SRC=`jar tf temp.jar | grep ".java"`
+rm temp.jar
+## end workaround
+
+javac -cp ../library/commons-cli-1.5.0.jar $SRC
+
+## unfortunately find is not supported by ci image
+# CLASS=`find . -name "*.class"`
+## so we have to missuse jar as a workaround
+jar cf temp.jar .
+CLASS=`jar tf temp.jar | grep ".class"`
+rm temp.jar
+## end workaround
+
+jar cfme ../md2021.jar ../manifest.txt mdw2021.CompetitionMainClass $CLASS
+
 cd ..
