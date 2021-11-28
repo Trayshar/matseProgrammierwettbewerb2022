965a1784c3b03d8bfdbb42d2032d76e6135f8408
Nicolas <nberr@eonerc.rwth-aachen.de>
2021-11-28 11:05:14 +0100

    moved source to src folder

---
+++ clean.sh
@@ -0,0 +1,2 @@
+rm -f md2021.jar
+rm -f src/mdw2021/*.class
--- compile.sh
+++ compile.sh
@@ -1,2 +1,4 @@
-javac -cp library/commons-cli-1.5.0.jar mdw2021/CompetitionMainClass.java 
-jar cfme md2021.jar manifest.txt mdw2021.CompetitionMainClass mdw2021/*.class
+cd src
+javac -cp ../library/commons-cli-1.5.0.jar mdw2021/CompetitionMainClass.java 
+jar cfme ../md2021.jar ../manifest.txt mdw2021.CompetitionMainClass mdw2021/*.class
+cd ..
