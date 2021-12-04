1944ed3100e4a208fc5d7889ca1dfc0e5ae7723e
Nicolas Berr <nberr@eonerc.rwth-aachen.de>
2021-12-04 09:14:11 +0100

    Added comments for Interface that has to be implemented

--- src/mdw2021/IPuzzle.java
+++ src/mdw2021/IPuzzle.java
@@ -1,8 +1,21 @@
 package mdw2021;
 
 public interface IPuzzle {
+
+	// Implement this function in a way that reads the input data with the given path/filename in the
+	// official input data format and imports it to your internal data structures
 	public void readInput(String filename);
+
+	// solve the puzzle defined by the input data
 	public void solve();
+
+	// This function shall return false, before a call of solve function.
+	// It shall return true, if solve was called and there actually is a solution.
+	// Otherwise false.
 	public boolean hasSolution();
+
+	// This function only is called if hasSolution() returns true.
+	// When called it shall write a solution to the output file with the given path/filename
+	// in the official output data format.
 	public void writeResult(String filename);
 }
